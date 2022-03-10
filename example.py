5cc2436e43097fdc18c8934b56a2e4567d8758c2
Roman <non oficial Novikov>
2022-03-10 10:55:34 +0300

    add example

---
+++ example.py
@@ -0,0 +1,30 @@
+
+
+
+# <- this is comments (code doesn`t executed)
+
+def main():   #<- this is main funtcion
+    variable1=8  #<- this is variable   type(int)
+    tmp_str="HELLO" #<- this is variable type(string)
+
+    tmp_arr=[1,2,3,8,9] #<- this is array int
+
+    for element in tmp_arr:
+        print("element "+str(element))
+
+
+    print(tmp_str)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
