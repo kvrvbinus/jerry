331f342a535c1c1466f2401666ef74ceebe928fa
kvrvbinus <101011383+kvrvbinus@users.noreply.github.com>
2022-03-11 13:55:12 +0400

    Ready

--- example.py
+++ example.py
@@ -1,30 +1,13 @@
+# <- this is comments (code doesnt execute)
+def main():   # <- this is main function
+    variable1 = 8  # <- this is variable   type(int)
 
 
-
-# <- this is comments (code doesn`t executed)
-
-def main():   #<- this is main funtcion
-    variable1=8  #<- this is variable   type(int)
-    tmp_str="HELLO" #<- this is variable type(string)
-
-    tmp_arr=[1,2,3,8,9] #<- this is array int
-
-    for element in tmp_arr:
-        print("element "+str(element))
-
-
-    print(tmp_str)
-
-
-
-
-
-
-
-
-
-
-
+tmp_str = "HELLO"  # <- this is variable type(string)
+tmp_arr = [1, 2, 3, 8, 9]  # <- this is array int
+for element in tmp_arr:
+    print("element "+str(element))
+print(tmp_str)
 
 if __name__ == '__main__':
     main()
